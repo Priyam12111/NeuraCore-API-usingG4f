2ca2b6df91882b8cc9b5f8199c38c2115133d98b
DrkCrypt <priyamtomar012@gmail.com>
2024-10-12 22:50:12 +0530

    Create gpt.py

---
+++ gpt.py
@@ -0,0 +1,18 @@
+from g4f.client import Client
+
+
+def sendMsg(msg):
+    try:
+        client = Client()
+        response = client.chat.completions.create(
+            # model="gpt-3.5-turbo",
+            model="gpt-4",
+            messages=[{"role": "user", "content": msg}],
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    print(sendMsg("""How much water should I give to tulsi daily? (No Chinese)"""))
